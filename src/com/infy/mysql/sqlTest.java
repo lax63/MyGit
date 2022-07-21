23b401e6575bc51662dde537004e3abfbb6198e0
lax63 <40056993+lax63@users.noreply.github.com>
2022-07-21 15:09:30 +0530

    Update sqlTest.java

--- src/com/infy/mysql/sqlTest.java
+++ src/com/infy/mysql/sqlTest.java
@@ -4,7 +4,7 @@ import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.ResultSet;
 import java.sql.Statement;
-
+//From Git
 public class sqlTest{
 	
 	public static void main(String []s) {
